a25d63e6dc1d3c6e28cd868ef45d74dc5cf53a14
Tosin Alliyu <oballiyu@haverford.edu>
2016-12-08 15:11:20 -0500

    cleaned up code and added lots of comments

--- XML_to_HTML.py
+++ XML_to_HTML.py
@@ -1,111 +1,177 @@
-import xml.etree.ElementTree as ET
 import os
 import re
+import shutil
 from lxml import etree
-import lxml.etree as ET
 from html5lib.sanitizer import HTMLSanitizerMixin
 from bs4 import BeautifulSoup
 
 
+
+    
 xml_file =  input( "Please insert path to XML file that you would like to convert to HTML:  ") 
-tree = etree.parse(xml_file)
+
+"""
+1 -- etree.parse(xml_file)
+reads the xml file that is given as input and changes it into an ElementTree object.
+This ElementTree object is an object that treats the XML file as a tree object
+with oarent and child nodes. 
+
+2 -- etree.tostring(tree)
+transforms the elementtree object to a python string object. 
+
+"""
+tree = etree.parse(xml_file)  
 xml_string = etree.tostring(tree)
 
+"""
+3 -- new_xml_string = xml_string[xml_string.index("</teiHeader>")+12:]
+Removes all TEI-specific heading context before and including the </teiHeader>
+closing tag.
 
+4 -- new_xml_string = new_xml_string.replace("</TEI>", "")
+Removes </TEI> closing tag (the opening tage was removed by the line above since
+it was before the </teiHeader> closing tag.
+
+"""
 new_xml_string = xml_string[xml_string.index("</teiHeader>")+12:]
 new_xml_string = new_xml_string.replace("</TEI>", "")
 
+"""  
+The four lines below take the string new_xml_string and save it to a file
+with the name 'converted'+[the file name of the original xml file that was given
+as input].
+
+The string new_xml_string is a string of the original xml file without some of
+the TEI specific tags that appear at the beginning and end of the original xml
+file.
+
+"""
 converted_file = os.path.basename(xml_file)
 text_file = open("converted_" + converted_file, "w")
 text_file.write(new_xml_string)
 text_file.close()
 
-       
-xml_toChange =  input("Please insert path to converted_xml_file.xml: ") #path to converted_xml_file.xml  
+#########################################################################
 
-new_tree = ET.parse(xml_toChange)
+"""
+This section of the code first gets the path of the file created above and then
+changes the file to a elementtree object.
+
+After turning the file into an elementtree the function changeXMLtag and the
+smaller for loops below, takes the xml tags in the file and changes its
+format so that it resembles an html tag.
+
+  changeXMLtag
+  
+  (1) for tagName in root.iter(str_tagname) -- gets all xml tags that have the same
+  tag name as the input {str_tagname}
+  (2) if len(tagName.items()) != 0 -- checks to see if a tag has any attributes
+      if the tag hass attributes, then
+      (3) tagName.attrib["key"] = "#"+ tagName.attrib["key"] -- takes the key
+      attribute of the tag and adds an hash to the front of its value
+  (4)-(5) for key in tagName.keys():
+            tagName.attrib["href"] = tagName.attrib.pop(key) --
+
+          changes the key attribute to a herf attribute
+  (6) tagName.attrib['class'] = tagName.tag -- adds the class attribute to the
+      tag and sets it to the tag name
+  (7) tagName.tag = "a" -- changes the tag name to an anchor tag
+
+
+"""
+xml_toChange = str(os.path.realpath("converted_" + converted_file))
+new_tree = etree.parse(xml_toChange)
 root = new_tree.getroot()
 
-for persName in root.iter("persName"):
-    if  len(persName.items()) != 0:
-        persName.attrib["key"] = "#"+persName.items()[0][1]
-    for key in persName.keys():
-        persName.attrib["href"] = persName.attrib.pop(key)
-    persName.attrib['class'] = persName.tag
-    persName.tag = "a"
+def changeXMLtag(str_tagname):
+  for tagName in root.iter(str_tagname):
+    if len(tagName.attrib) != 0:
+      tagName.attrib["key"] = "#"+ tagName.items()[0][1]
+    for key in tagName.keys():
+      tagName.attrib["href"] = tagName.attrib.pop(key)
+    tagName.attrib['class'] = tagName.tag
+    tagName.tag = "a"
+
+tags_to_change = ["persName", "placeName", "orgName", "geogName"] 
+for tags in tags_to_change:
+    changeXMLtag(tags)
     
-for placeName in root.iter("placeName"):
-    if  len(placeName.items()) != 0:
-        placeName.attrib["key"] = "#"+placeName.items()[0][1]
-    for key in placeName.keys():
-        placeName.attrib["href"] = placeName.attrib.pop(key)
-    placeName.attrib['class'] = placeName.tag
-    placeName.tag = "a"
+"""
+The for loops below
 
-for orgName in root.iter("orgName"):
-    if  len(orgName.items()) != 0:
-        orgName.attrib["key"] = "#"+orgName.items()[0][1]
-    for key in orgName.keys():
-        orgName.attrib["href"] = orgName.attrib.pop(key)
-    orgName.attrib['class'] = orgName.tag
-    orgName.tag = "a"
-
-for geogName in root.iter("geogName"):
-    if  len(geogName.items()) != 0:
-        geogName.attrib["key"] = "#"+geogName.items()[0][1]
-    for key in geogName.keys():
-        geogName.attrib['href'] = geogName.attrib.pop(key)
-    geogName.attrib['class'] = geogName.tag
-    geogName.tag = "a"
-
+  (1) takes the date xml tag and changes it to an anchor tag
+  (2) takes the title xml tag and changes it to a h1 tag
+  (3) takes the row xml tag and changes it to the appropriate html
+      table row tag
+  
+"""
 for date in root.iter("date"):
     date.tag = "a"
-    
-for line_break in root.iter("lb"):
-    line_break.tag = "br"
-    line_break.attrib.clear()
-    
-for page_break in root.iter("pb"):
-    for attr in page_break.attrib:
-        if attr != 'n':
-            del page_break.attrib[attr]
-    
 
 for title in root.iter("title"):
     title.tag = "h1"
 
 for row in root.iter("row"):
     row.tag = "tr"
+    
+"""
+The for loops below
 
+  (1) takes the line break xml tag, changes it to the appropriate html line
+      break tag and then clears the attribute of the tag
+  (2) takes the cell xml tag, changes it to the appropriate html table cell
+      tag and then clears the attribute of the tag
+"""
+for line_break in root.iter("lb"):
+    line_break.tag = "br"
+    line_break.attrib.clear()
+    
 for cell in root.iter("cell"):
     cell.tag = "td"
     cell.attrib.clear()
     
-new_tree.write(xml_toChange)
+"""
+The for loop below takes the xml page break tag and deletes all attributes that
+is not a n (page number) attribute.
+"""
+for page_break in root.iter("pb"):
+    for attr in page_break.attrib:
+        if attr != 'n':
+            del page_break.attrib[attr]
 
-another_tree = etree.parse(xml_toChange) #parses xml file
-xml_string = etree.tostring(another_tree) #converts parsed xml file to string 
+#########################################################################
+            
+new_tree.write(xml_toChange) #updates the changes made above in the file 
+another_tree = etree.parse(xml_toChange) #parses xml file 
 
+list_of_xml_tags = [elem.tag for elem in root.iter() if elem is not root] #gets all tags in the xml file 
 
-
-list_of_xml_tags = [elem.tag for elem in root.iter() if elem is not root]
-
-for tag in list_of_xml_tags:
+for tag in list_of_xml_tags:#takes out all tags that are not approporiate html tags from the xml file except for the root, body tag and pg tag
     if tag not in HTMLSanitizerMixin.acceptable_elements and tag != "body" and tag != "pb" :
-        etree.strip_tags(another_tree, tag) #takes out tags that are not html tags from xml (changing to html) file
+        etree.strip_tags(another_tree, tag) 
         
 
-another_tree.write(xml_toChange)#changes the file saved as xml_toChange so that it does not include unrecognized HTML tags
+another_tree.write(xml_toChange)#saves the changes made to the file xml_toChange
 
-
+#adds html header to xml_toChange and writes to new file, which is essentially now a html file
 with open(xml_toChange, 'rb') as f, open('new_'+ converted_file,  'wb') as g:
     g.write('<!DOCTYPE html><html>{}</html>'.format(f.read()))
-    #adds html header to xml_toChange and writes to new file, which is essentially now converted to a html file
+    
+#deletes the file saved as 'converted'+converted_file from directoy because
+#it is not needed anymore (can remove if you want to save this file)
+os.remove(xml_toChange)
 
-#MAKE SURE TO COPY FILE THAT WAS JUST CREATED AND SAVE WITH AN HTML EXTENSIO
+#makes a copy of the recently created file and saves as html file
+xml_file = os.path.realpath('new_'+ converted_file)
+html_file = xml_file[:len(xml_file)-3]+"html"
+shutil.copy2(xml_file,html_file)
+
 from page_break_csv import csv_page_break
 
-csv_page_break()
+#saves the contents of a html file to a csv where each cell is
+#comprised of the content within a page break in the xml file
+csv_page_break(xml_file, html_file)
+
 
 
 
--- page_break_csv.py
+++ page_break_csv.py
@@ -5,38 +5,51 @@ from bs4 import BeautifulSoup
 import csv
 
 
-##xml_file = input( "Please insert path to HTML file with XML extension:")
-##html_file = input("Please insert path to same file with a HTML extension:")
-
-def csv_page_break():
+def csv_page_break(xml_file, html_file):
       
-      xml_file = input( "Please insert path to XML file:")
-      html_file = input("Please inser path to same file with a html extension:") #need the xml file saved as a html file because of beautiful soup
-
-      #soup = BeautifulSoup(open('/Users/oluwatosinalliyu/Desktop/quakers_and_indians/testing_files/new_Letterscopy.html'),"html.parser")
+      #formats the html page so that it is easier to read
       soup = BeautifulSoup(open(html_file, 'rb'), "html.parser")
-      page_break_loc = soup.find_all('pb') #collects all pb tags into a list. list is composed of pb tag with closing tag for each pb tag
 
-      #the next two for loops is to properly format the pb takes in the page_break_loc so that it looks the same as the original pb tag in the xml page
-      pb_list = []
-
-      for x in page_break_loc:
-            pb_list.append(re.sub('\</pb>$', '',str(x)))
-            
-      for pb in page_break_loc: #removes the closing tag
-            pb_list.append(re.sub('\</pb>$', '',str(pb)))
-
+      """
+      collects all pb tags into a list where each pb tag is given a opening
+      and closing tag
+      """
+      page_break_loc = soup.find_all('pb')
+      
+      """
+      the next for loop re-formats the pb tags so that they look like
+      the pb tags in the xml file.
+            (1) new_pb = re.sub('\</pb>$', '',str(pb)) -- removes the closing
+                  pb tag
+            (2) new_pb = new_pb[:-1]+"/"+new_pb[-1] -- adds a backslash to the
+                  end of the string in the pb tag
+      """
       new_pb_list = []
-      for ele in pb_list:#adds a backslash to the end of the string pb within the <>
-            ele = ele[:-1]+"/"+ele[-1]
-            new_pb_list.append(ele)
-      #print new_pb_list
-      #tree = etree.parse(open('/Users/oluwatosinalliyu/Desktop/forpage_break.xml'))
-      #changing the xml file to a string so that it can be easilt parsed through 
+      
+      for pb in page_break_loc: 
+            new_pb = re.sub('\</pb>$', '',str(pb))
+            new_pb = new_pb[:-1]+"/"+new_pb[-1]
+            new_pb_list.append(new_pb)
+
+            
+      """
+      opens the xml file, first changes it to a elementtree object and then a
+      string.
+      """ 
       tree = etree.parse(open(xml_file))
       xml_string = etree.tostring(tree)
 
-
+      """
+       The code below finds each page break in the xml file and saves the content between
+       each break into separate rows in a csv so that all contents within a page
+       as indicated in the xml file is saved to a row in the csv.
+             (1) pb_csv_list.append(os.path.basename(xml_file)) -- the first column in the
+                 csv has the name of the file that is being read
+             (2) pb_csv_list.append(os.path.basename(xml_file)+"_pg_"+ str(count)) -- the second
+                 column has the current page that is being read within the xml file
+             (3) pb_csv_list.append(xml_string[:xml_string.find(pbs)]) -- the third column has the
+                 page content
+      """
       with open("csv_for_"+os.path.basename(xml_file)+".csv", 'wb') as f:
             writer = csv.writer(f)
             count = 0
@@ -51,9 +64,12 @@ def csv_page_break():
                   
                   count+=1
                   xml_string = xml_string[xml_string.find(pbs)+len(pbs):]
-            #print xml_string
+           
 
-
+            """
+            gets the remaining content in the xml page and stores it in the csv with the
+            same format
+            """
             pb_csv_list = []
             pb_csv_list.append(os.path.basename(xml_file))
             pb_csv_list.append(os.path.basename(xml_file)+"_pg_"+ str(count))
@@ -61,13 +77,8 @@ def csv_page_break():
             csv_list.append(pb_csv_list)
             
             writer.writerows(csv_list)
-
-'''
-if re.search(r' n="(\d+)"', pbs) != None:
-                  pb_csv_list.append(os.path.basename(xml_file)+str(re.search(r' n="(\d+)"', pbs).group(1)))
-            else:
-                  pb_csv_list.append(os.path.basename(xml_file)+ str(count))
-
-'''
+            
+            os.remove(xml_file) #removes xml_file from directoy (can delete if you want to save this file)
+            os.remove(html_file) #removes html_file from directoy (can delete if you want to save file)
            
       
